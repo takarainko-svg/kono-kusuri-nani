468e00a306c30be1e9a3bec2a9eace8480028839
takarainko-svg <takarainko@kou1973ou2202ki2204.com>
2026-09-09 15:46:01 +0900

    本番版リリース用にキャッシュを更新

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME="kono-kusuri-nani-v1";
+const CACHE_NAME="kono-kusuri-nani-v1.0.0";
 const ASSETS=["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
 self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))));self.clients.claim();});
